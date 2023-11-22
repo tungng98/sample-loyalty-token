fa2c30c21db4d7f78227830f2c490f177e0ef043
Tung Nguyen <87560224+tungng98@users.noreply.github.com>
2023-11-22 14:34:47 +0700

    init repository

---
+++ hardhat.config.ts
@@ -0,0 +1,107 @@
+import '@nomicfoundation/hardhat-toolbox';
+import dotenv from 'dotenv';
+import 'hardhat-dependency-compiler';
+import 'hardhat-deploy';
+import 'hardhat-gas-reporter';
+import { HardhatUserConfig } from 'hardhat/config';
+
+dotenv.config();
+
+const privateKey = process.env.PRIVATE_KEY || '0x0000000000000000000000000000000000000000000000000000000000000000';
+const infuraKey = process.env.INFURA_API_KEY || '';
+const etherscanApi = process.env.ETHERSCAN_API_KEY || '';
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: '0.6.12',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+      {
+        version: '0.4.25',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: false,
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${infuraKey}`,
+      accounts: [privateKey]
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${infuraKey}`,
+      accounts: [privateKey]
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${infuraKey}`,
+      accounts: [privateKey]
+    },
+    arbitrumRinkeby: {
+      url: `https://arbitrum-rinkeby.infura.io/v3/${infuraKey}`,
+      accounts: [privateKey]
+    },
+    arbitrum: {
+      url: `https://arbitrum-mainnet.infura.io/v3/${infuraKey}`,
+      accounts: [privateKey]
+    },
+    bnb: {
+      url: `https://bsc-dataseed.binance.org/`,
+      accounts: [privateKey]
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${infuraKey}`,
+      accounts: [privateKey]
+    },
+    optimism: {
+      url: `https://optimism-mainnet.infura.io/v3/${infuraKey}`,
+      accounts: [privateKey]
+    },
+    optimismKovan: {
+      url: `https://optimism-kovan.infura.io/v3/${infuraKey}`,
+      accounts: [privateKey]
+    },
+    polygon: {
+      url: `https://polygon-mainnet.infura.io/v3/${infuraKey}`,
+      accounts: [privateKey]
+    },
+  },
+  etherscan: {
+    apiKey: etherscanApi
+  },
+  paths: {
+    artifacts: './artifacts',
+    cache: './cache',
+    deploy: './deploy',
+    sources: './contracts',
+    tests: './tests',
+  },
+  dependencyCompiler: {
+    paths: [
+    ],
+  },
+  gasReporter: {
+    currency: 'USD',
+    enabled: true,
+    gasPrice: 10,
+    token: 'ETH',
+  },
+  mocha: {
+    timeout: 60000
+  }
+};
+
+export default config;
